621d823e278d395887e6485ff7287671def6810a
Juanfran <juanfran.ag@gmail.com>
2020-02-17 21:26:10 +0100

    to post

--- server/plugin.go
+++ server/plugin.go
@@ -42,12 +42,13 @@ func generateID() string {
 }
 
 func serveImg(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("aaaaaa")
 	w.Header().Set("Content-Type", "image/jpeg")
 	w.Header().Set("Cache-Control", "public, max-age=604800")
 
 	vars := mux.Vars(r)
 	memeID := vars["name"]
-
+	fmt.Println("bbbb")
 	if err := jpeg.Encode(w, generatedMemes[memeID], &jpeg.Options{
 		Quality: 90,
 	}); err != nil {
@@ -57,26 +58,27 @@ func serveImg(w http.ResponseWriter, r *http.Request) {
 }
 
 func (p *Plugin) generateMeme(faces []*facesLib.FaceType) (image.Image, error) {
+	fmt.Println("6666")
 	link2, errReadFile := p.API.ReadFile(currentImagePath)
 	if errReadFile != nil {
 		return nil, errors.New(errReadFile.Error())
 	}
-
+	fmt.Println("777")
 	img, _, errDecodeImage := image.Decode(bytes.NewReader(link2))
 	if errDecodeImage != nil {
 		return nil, errDecodeImage
 	}
-
+	fmt.Println("888")
 	bundlePath, errGetBundlePath := p.API.GetBundlePath()
 	if errGetBundlePath != nil {
 		return nil, errGetBundlePath
 	}
-
+	fmt.Println("999")
 	cascadeFile, errReadCascade := ioutil.ReadFile(filepath.Join(bundlePath, "assets", "facefinder"))
 	if errReadCascade != nil {
 		return nil, errReadCascade
 	}
-
+	fmt.Println("00000")
 	return swap.ImgFaceSwap(img, faces, cascadeFile)
 }
 
@@ -143,16 +145,17 @@ func (p *Plugin) ExecuteCommand(c *plugin.Context, args *model.CommandArgs) (*mo
 	}
 
 	if len(memeFaces) > 0 {
+		fmt.Println("11111")
 		id := generateID()
-
+		fmt.Println("222")
 		img, err := p.generateMeme(memeFaces)
-
+		fmt.Println("333")
 		if err != nil {
 			return nil, model.NewAppError("ExecuteCommand", "error faceswap", nil, err.Error(), http.StatusInternalServerError)
 		}
 
 		generatedMemesIds = append(generatedMemesIds, id)
-
+		fmt.Println("4444")
 		if len(generatedMemesIds) > maxInMemoryMemes {
 			delete(generatedMemes, generatedMemesIds[0])
 			generatedMemesIds = generatedMemesIds[1:]
@@ -160,10 +163,38 @@ func (p *Plugin) ExecuteCommand(c *plugin.Context, args *model.CommandArgs) (*mo
 
 		generatedMemes[id] = img
 
-		return &model.CommandResponse{
-			ResponseType: model.COMMAND_RESPONSE_TYPE_IN_CHANNEL,
-			Text:         "![Face swap](/plugins/faceswap/img/" + id + ".jpg)",
-		}, nil
+		buf := new(bytes.Buffer)
+		jpeg.Encode(buf, img, nil)
+		imgBytes := buf.Bytes()
+
+		fmt.Println("555")
+		fmt.Printf("image path: %v", "/plugins/faceswap/img/"+id+".jpg")
+
+		imgInfo, errUpload := p.API.UploadFile(imgBytes, args.ChannelId, id+".jpg")
+
+		if errUpload != nil {
+			return nil, model.NewAppError("ExecuteCommand", "error faceswap", nil, errUpload.Error(), http.StatusInternalServerError)
+		}
+
+		var fileIDs []string
+		fileIDs = append(fileIDs, imgInfo.Id)
+
+		post := &model.Post{
+			UserId:    args.UserId,
+			ChannelId: args.ChannelId,
+			RootId:    args.RootId,
+			FileIds:   fileIDs,
+		}
+
+		_, createPostError := p.API.CreatePost(post)
+
+		if createPostError != nil {
+			return nil, model.NewAppError("ExecuteCommand", "error faceswap", nil, createPostError.Error(), http.StatusInternalServerError)
+		}
+
+		// p.API.SendEphemeralPost(args.UserId, post)
+
+		return &model.CommandResponse{}, nil
 	}
 
 	return &model.CommandResponse{
@@ -180,7 +211,22 @@ func (p *Plugin) MessageHasBeenPosted(c *plugin.Context, post *model.Post) {
 		if err != nil {
 			fmt.Println(err.Error())
 		} else if utils.IsImage(link.Path) {
+			fmt.Println("NEW IMAGE")
+			fmt.Println(link.Path)
 			currentImagePath = link.Path
 		}
 	}
 }
+
+// ,"msg":"NEW IMAGE","plugin_id":"faceswap","source":"plugin_stdout"}
+// ,"msg":"20200217/teams/noteam/channels/eeyrbxyqu3dduy7ukm99uqepyo/users/9zbb9jj73igzdxz5cy4je7xa9y/xphospotqidkbp34z9fmpeon1y/meme2.jpg","plugin_id":"faceswap","source":"plugin_stdout"}
+// ,"msg":"11111","plugin_id":"faceswap","source":"plugin_stdout"}
+// "msg":"222","plugin_id":"faceswap","source":"plugin_stdout"}
+// ,"msg":"6666","plugin_id":"faceswap","source":"plugin_stdout"}
+// ,"msg":"777","plugin_id":"faceswap","source":"plugin_stdout"}
+// ,"msg":"888","plugin_id":"faceswap","source":"plugin_stdout"}
+// "msg":"999","plugin_id":"faceswap","source":"plugin_stdout"}
+// ,"msg":"00000","plugin_id":"faceswap","source":"plugin_stdout"}
+// ,"msg":"333","plugin_id":"faceswap","source":"plugin_stdout"}
+// ,"msg":"4444","plugin_id":"faceswap","source":"plugin_stdout"}
+// "msg":"555","plugin_id":"faceswap","source":"plugin_stdout"}
