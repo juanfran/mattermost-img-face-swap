42de447b2b13c93dad588c86a3c374d1323f11e8
Juanfran <juanfran.ag@gmail.com>
2020-02-18 16:38:58 +0100

    cleaning repo

--- server/plugin_test.go
+++ server/plugin_test.go
@@ -4,19 +4,4 @@ import (
 	"testing"
 )
 
-func TestServeHTTP(t *testing.T) {
-	// assert := assert.New(t)
-	// plugin := Plugin{}
-	// w := httptest.NewRecorder()
-	// r := httptest.NewRequest(http.MethodGet, "/", nil)
-
-	// plugin.ServeHTTP(nil, w, r)
-
-	// result := w.Result()
-	// assert.NotNil(result)
-	// bodyBytes, err := ioutil.ReadAll(result.Body)
-	// assert.Nil(err)
-	// bodyString := string(bodyBytes)
-
-	// assert.Equal("Hello, world!", bodyString)
-}
+func TestServeHTTP(t *testing.T) {}
